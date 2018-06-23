c532682cf4472a929c58f32ab36fafe5f271afac
Alan Justino <alan.justino@yahoo.com.br>
2018-06-23 19:17:51 -0300

    Bump version: 0.1.8 → 0.2.0

--- grumpy-tools-src/setup.py
+++ grumpy-tools-src/setup.py
@@ -53,7 +53,7 @@ if needs_pytest:
 
 
 COMMON_OPTIONS = dict(
-    version='0.1.8',
+    version='0.2.0',
     description="Grumpy Runtime & Transpiler",
     long_description=readme,
     author="Dylan Trotter et al.",
