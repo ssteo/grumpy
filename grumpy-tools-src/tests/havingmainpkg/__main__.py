790ed623620c7de731809668b7f66204a4879777
cclauss <cclauss@bluewin.ch>
2018-09-02 18:29:53 +0200

    print() is a function in Python 3

--- grumpy-tools-src/tests/havingmainpkg/__main__.py
+++ grumpy-tools-src/tests/havingmainpkg/__main__.py
@@ -1,13 +1,14 @@
-import sys
+from __future__ import print_function
+
 
 def main():
-    print '__name__ is', __name__
-    print '__package__ is', __package__
+    print('__name__ is', __name__)
+    print('__package__ is', __package__)
     from . import SPAM
     print(SPAM)
 
 
 if __name__ == '__main__':
-    print '__name__ IS __main__'
-    print '__package__ is', __package__
+    print('__name__ IS __main__')
+    print('__package__ is', __package__)
     main()
